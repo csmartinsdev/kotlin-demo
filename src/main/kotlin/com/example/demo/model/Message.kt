e9dc9ee899f2cad52e9385963556a2062383d113
Carlos Martins <cmartins.dev@gmail.com>
2025-09-10 16:33:46 -0300

    Applied change in the table name

--- src/main/kotlin/com/example/demo/model/Message.kt
+++ src/main/kotlin/com/example/demo/model/Message.kt
@@ -3,5 +3,5 @@ package com.example.demo.model
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
-@Table("messages")
+@Table("MESSAGES")
 data class Message(val text: String, @Id val id: String? = null)
